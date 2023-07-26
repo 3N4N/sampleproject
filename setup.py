20f6edd8fdeeb8dce2307b2234a3c63c9d4c12a9
Enan Ajmain <3nan.ajmain@gmail.com>
2023-07-26 12:40:08 +0600

    chore: update for setuptools-movandi

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
 
 def setup_for_python36():
-    import setuptools_movandi
-    from setuptools_movandi import parse_dependencies, parse_requirements
+    from setuptools_movandi import ppsetuptools
+    from setuptools_movandi.dependency_parser import parse_requirements
+    from setuptools_movandi.version_schemer import version_scheme
     import os
-    cwd = os.getcwd()
-    setuptools_movandi.setup(
-        install_requires = parse_requirements(os.path.join(cwd,'requirements.txt')),
+    rootdir = os.getcwd()
+    ppsetuptools.setup(
+        install_requires = parse_requirements(rootdir, os.path.join(rootdir,'requirements.txt')),
+        # use_scm_version={
+        #     "git_describe_command": 'git describe --tags --long --match "v*"',
+        #     "version_scheme": version_scheme,
+        #     "local_scheme": "no-local-version",
+        #     "relative_to": __file__,
+        #     "write_to": f"{rootdir}/src/sample/_version.py",
+        # },
     )
 
 if setuptools.__version__ >= '61':
